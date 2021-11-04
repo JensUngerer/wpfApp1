3712bfb86b70e9556e502ce1f1065a371a2c4b1d
Jens Ungerer <github@jens-ungerer.de>
2021-11-04 04:00:19 +0100

    adding dependency and attached properties

--- WpfApp1/PropertyChanged.cs
+++ WpfApp1/PropertyChanged.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace WpfApp1
 {
@@ -21,5 +22,50 @@ namespace WpfApp1
                 throw new NotImplementedException();
             }
         }
+
+
+
+        public int TheDependencyProperty
+        {
+            get { return (int)GetValue(TheDependencyPropertyProperty); }
+            set { NewMethod(value); }
+        }
+
+        private static void NewMethod(int value)
+        {
+            SetValue(TheDependencyPropertyProperty, value);
+        }
+
+        private static void SetValue(DependencyProperty theDependencyPropertyProperty, int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int GetValue(DependencyProperty theDependencyPropertyProperty)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Using a DependencyProperty as the backing store for TheDependencyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TheDependencyPropertyProperty =
+            DependencyProperty.Register("TheDependencyProperty", typeof(int), typeof(PropertyChanged), new PropertyMetadata(TheDependencyPropertyProperty));
+
+
+
+
+        public static int GetAttachedProperty(DependencyObject obj)
+        {
+            return (int)obj.GetValue(AttachedPropertyProperty);
+        }
+
+        public static void SetAttachedProperty(DependencyObject obj, int value)
+        {
+            obj.SetValue(AttachedPropertyProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for AttachedProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AttachedPropertyProperty =
+            DependencyProperty.RegisterAttached("AttachedProperty", typeof(int), typeof(PropertyChanged), new PropertyMetadata(0));
+
     }
 }
