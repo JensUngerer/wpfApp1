c43a621fc1ecd558cc83d1e7770403d76c4830be
Jens Ungerer <github@jens-ungerer.de>
2021-11-04 03:49:57 +0100

    initial commit

---
+++ WpfApp1/PropertyChanged.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class PropertyChanged : INotifyPropertyChanged
+    {
+        event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
+        {
+            add
+            {
+                throw new NotImplementedException();
+            }
+
+            remove
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ WpfApp1/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class ViewModel
+    {
+        private int _jens = 10;
+
+        public int Jens
+        {
+            get { return _jens; }
+            set { _jens = value; }
+        }
+
+    }
+}
